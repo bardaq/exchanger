6893477dff4f5c36959bc1531ea6ef8ce8b42258
bardaq <bardaq@icloud.com>
2018-05-19 21:44:12 +0300

    Checkbox terms link routing fixed

--- public/app/components/ExchangeForm/fields/TermsCheck.js
+++ public/app/components/ExchangeForm/fields/TermsCheck.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { InputGroup, Label, Input} from 'reactstrap';
+import { Link } from "react-router-dom";
 import MaskedInput from 'react-maskedinput'
 
 export default class TermsCheck extends React.Component {
@@ -14,10 +15,8 @@ export default class TermsCheck extends React.Component {
 
 	render() {
 		return <InputGroup className={ this.props.isInvalid ? 'hasError' : '' }>
-			<Label className="exchange-form__checkbox-label">Отправляя форму вы подтверждаете, что введенные данные верны и вы ознакомлены и согласены с условиями <a href="#">Соглашения</a> </Label>
+			<Label className="exchange-form__checkbox-label">Отправляя форму вы подтверждаете, что введенные данные верны и вы ознакомлены и согласены с условиями <Link to='/terms'>Соглашения</Link> </Label>
 			<Input type="checkbox" className="exchange-form__checkbox" onClick={ e => this.clickHandler(e) }/>
 		</InputGroup>
 	}
 }
-
-
